73299309984b24c461d9d0544889a008f609b28b
Mitch Tabian <mitch@tabian.ca>
2021-03-29 13:01:09 -0700

    fix missing files?

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -24,13 +24,6 @@ android {
 }
 kotlin {
     android()
-//    ios {
-//        binaries {
-//            framework {
-//                baseName = "shared"
-//            }
-//        }
-//    }
     val sdkName: String? = System.getenv("SDK_NAME")
     val isiOSDevice = sdkName.orEmpty().startsWith("iphoneos")
     if (isiOSDevice) {
@@ -83,21 +76,6 @@ kotlin {
 
     }
 }
-
-//val packForXcode by tasks.creating(Sync::class) {
-//    group = "build"
-//    val mode = System.getenv("CONFIGURATION") ?: "DEBUG"
-//    val sdkName = System.getenv("SDK_NAME") ?: "iphonesimulator"
-//    val targetName = "ios" + if (sdkName.startsWith("iphoneos")) "Arm64" else "X64"
-//    val framework = kotlin.targets.getByName<KotlinNativeTarget>(targetName).binaries.getFramework(mode)
-//    inputs.property("mode", mode)
-//    dependsOn(framework.linkTask)
-//    val targetDir = File(buildDir, "xcode-frameworks")
-//    from({ framework.outputDirectory })
-//    into(targetDir)
-//}
-
-//tasks.getByName("build").dependsOn(packForXcode)
 
 sqldelight {
     database("RecipeDatabase") {
