62e5d5b457d76fc7e16096ec64791deb8c66dac7
Mitch Tabian <mitch@tabian.ca>
2021-03-29 10:16:15 -0700

    got the native driver to import thanks for john O'Reilly

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -24,12 +24,19 @@ android {
 }
 kotlin {
     android()
-    ios {
-        binaries {
-            framework {
-                baseName = "shared"
-            }
-        }
+//    ios {
+//        binaries {
+//            framework {
+//                baseName = "shared"
+//            }
+//        }
+//    }
+    val sdkName: String? = System.getenv("SDK_NAME")
+    val isiOSDevice = sdkName.orEmpty().startsWith("iphoneos")
+    if (isiOSDevice) {
+        iosArm64("ios")
+    } else {
+        iosX64("ios")
     }
     sourceSets {
         val ktor_version = "1.5.2"
@@ -77,20 +84,20 @@ kotlin {
     }
 }
 
-val packForXcode by tasks.creating(Sync::class) {
-    group = "build"
-    val mode = System.getenv("CONFIGURATION") ?: "DEBUG"
-    val sdkName = System.getenv("SDK_NAME") ?: "iphonesimulator"
-    val targetName = "ios" + if (sdkName.startsWith("iphoneos")) "Arm64" else "X64"
-    val framework = kotlin.targets.getByName<KotlinNativeTarget>(targetName).binaries.getFramework(mode)
-    inputs.property("mode", mode)
-    dependsOn(framework.linkTask)
-    val targetDir = File(buildDir, "xcode-frameworks")
-    from({ framework.outputDirectory })
-    into(targetDir)
-}
+//val packForXcode by tasks.creating(Sync::class) {
+//    group = "build"
+//    val mode = System.getenv("CONFIGURATION") ?: "DEBUG"
+//    val sdkName = System.getenv("SDK_NAME") ?: "iphonesimulator"
+//    val targetName = "ios" + if (sdkName.startsWith("iphoneos")) "Arm64" else "X64"
+//    val framework = kotlin.targets.getByName<KotlinNativeTarget>(targetName).binaries.getFramework(mode)
+//    inputs.property("mode", mode)
+//    dependsOn(framework.linkTask)
+//    val targetDir = File(buildDir, "xcode-frameworks")
+//    from({ framework.outputDirectory })
+//    into(targetDir)
+//}
 
-tasks.getByName("build").dependsOn(packForXcode)
+//tasks.getByName("build").dependsOn(packForXcode)
 
 sqldelight {
     database("RecipeDatabase") {
--- shared/src/iosMain/kotlin/com/example/kmmplayground/shared/cache/RecipeDatabase.kt
+++ shared/src/iosMain/kotlin/com/example/kmmplayground/shared/cache/RecipeDatabase.kt
@@ -2,6 +2,7 @@ package com.example.kmmplayground.shared.cache
 
 import com.example.kmmplayground.cache.RecipeDatabase
 import com.squareup.sqldelight.db.SqlDriver
+import com.squareup.sqldelight.drivers.native.NativeSqliteDriver
 
 actual class DriverFactory {
     actual fun createDriver(): SqlDriver {
